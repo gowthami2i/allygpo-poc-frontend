90ddf77f3dc19f5990db2034fd4fda802139ac5a
gowthami2i <gowtham.p@ideas2it.com>
2024-10-03 12:11:34 +0530

    feature button and dropdown

--- src/layouts/header/header.tsx
+++ src/layouts/header/header.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import AllyGpo_icon from "../../assets/images/png/AllyIQ_Icon.png";
+// import AllyGpo_icon from "../../assets/images/svg/ally_gpo_logo.svg";
 import "./header.scss";
 import Typography from "../../typography/Typography";
 
@@ -7,10 +7,13 @@ export const Header = () => {
   return (
     <div className="header-container">
       <div className="header-primary-container flex align-items-center justify-content-center">
-        <img src={AllyGpo_icon} className="allgpoIcon" />
+        <img src={AllyGpo_icon} alt="allgpoIcon" width={"80px"} />
       </div>
-      <div className="header-secondary-container px-2">
-        <Typography variant={"h6"} className="text-white m-0 p-3">
+      <div className="header-secondary-container px-2 w-full">
+        <Typography
+          variant={"h6"}
+          className="text-white m-0 font-semibold p-3 pl-5"
+        >
           Contract Explorer
         </Typography>
       </div>
